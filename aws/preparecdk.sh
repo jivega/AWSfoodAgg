baf6253d97cd0153f1438d097ffcc55fd835da2d
jivega <jivega@yahoo.com>
2022-06-18 01:13:51 +0200

    Create preparecdk.sh

---
+++ aws/preparecdk.sh
@@ -0,0 +1,9 @@
+mkdir tesingcdk
+cd testingcdk
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade virtualenv
+
+
+python3 -m pip install aws-cdk-lib
+	
