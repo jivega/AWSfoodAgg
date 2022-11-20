e9e1ab7c9b314bc7f4c845bff8697286b3b73fbf
Jorge <jivega@yahoo.com>
2022-11-21 00:46:58 +0100

    Include scala class

---
+++ src/main/scala/com/jivega/awsfoodagg/driver/Driver.scala
@@ -0,0 +1,14 @@
+package com.jivega.awsfoodagg.driver
+
+object Driver {
+
+  def main(args: Array[String]): Unit = {
+    println(hello)
+
+  }
+  def hello(): String = {
+    "Hello, world!"
+  }
+
+
+}
