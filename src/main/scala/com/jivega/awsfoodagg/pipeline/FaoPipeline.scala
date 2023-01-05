97b861fccc2b2097dc70a376bff901898339bb98
Jorge <jorgeawstest@gmail.com>
2023-01-05 11:39:30 +0000

    Include shell test30

--- src/main/scala/com/jivega/awsfoodagg/pipeline/FaoPipeline.scala
+++ src/main/scala/com/jivega/awsfoodagg/pipeline/FaoPipeline.scala
@@ -21,9 +21,9 @@ object FaoPipeline {
     df.filter(df("Item Code (CPC)") === "'01802"  && df("Year") === "2020" ).sort(col("Value").desc).show(300,false)
     
     // Spain Area '724
-    df.filter(df("Item Code (CPC)") === "'01802"  && df("Area Code (M49)") === "'724" ).sort(col("Year").desc).show(300,false)
+    df.filter(df("Item Code (CPC)") === "'01802"  && df("Area Code (M49)") === "'724" ).sort(col("Year").desc,col("Element").desc).show(300,false)
     // Europe '150 
-    df.filter(df("Item Code (CPC)") === "'01802"  && df("Area Code (M49)") === "'150" ).sort(col("Year").desc).show(300,false)
+    df.filter(df("Item Code (CPC)") === "'01802"  && df("Area Code (M49)") === "'150" ).sort(col("Year").desc,col("Element").desc).show(300,false)
     
     df.filter(df("Year") === "2020" ).sort(col("Value").desc).show(200,false)
   }
