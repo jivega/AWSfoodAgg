23b11c9619269d7ddc73f5011cd650b67793e3a6
Jorge <jorgeawstest@gmail.com>
2023-01-05 01:17:15 +0000

    Include shell test22

--- src/main/scala/com/jivega/awsfoodagg/pipeline/FaoPipeline.scala
+++ src/main/scala/com/jivega/awsfoodagg/pipeline/FaoPipeline.scala
@@ -1,6 +1,6 @@
 package com.jivega.awsfoodagg.pipeline
 import org.apache.spark.sql.DataFrame 
-import org.apache.spark.sql.functions.{count,lit}
+import org.apache.spark.sql.functions.{count,lit,col}
 import org.apache.spark.sql.SparkSession
 
 object FaoPipeline {
@@ -11,6 +11,8 @@ object FaoPipeline {
     df.groupBy("Item Code","Item Code (CPC)","Item").agg(count(lit(1)).alias("NumOfItems")).sort("Item","Item Code","Item Code (CPC)").show(500,false)
     df.groupBy("Element Code","Element").agg(count(lit(1)).alias("NumOfElements")).sort("Element","Element Code").show(500,false)
     df.groupBy("Unit").agg(count(lit(1)).alias("NumOfUnits")).sort("Unit").show(300,false)
+    // Top Olive Oil '2167
+    df.filter(df("Item Code (CPC)") === "'2167").sort(col("Value").desc).show(300,false)
     
   }
 }
