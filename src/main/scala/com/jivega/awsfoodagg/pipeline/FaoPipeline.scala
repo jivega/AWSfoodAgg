0f2007ea14cac8dc0bc7126767a4fa04dff12d16
Jorge <jorgeawstest@gmail.com>
2023-01-05 00:33:14 +0000

    Include shell test21

--- src/main/scala/com/jivega/awsfoodagg/pipeline/FaoPipeline.scala
+++ src/main/scala/com/jivega/awsfoodagg/pipeline/FaoPipeline.scala
@@ -7,7 +7,10 @@ object FaoPipeline {
 
   def qualityData(df: DataFrame, spark: SparkSession ): Unit = {
     println("Quality")
-    df.groupBy("Area Code","Area Code (M49)","Area").agg(count(lit(1)).alias("NumOfArea")).sort("Area","Area Code","Area Code (M49)").show(300,false)
+    df.groupBy("Area Code","Area Code (M49)","Area").agg(count(lit(1)).alias("NumOfArea")).sort("Area","Area Code","Area Code (M49)").show(500,false)
+    df.groupBy("Item Code","Item Code (CPC)","Item").agg(count(lit(1)).alias("NumOfItems")).sort("Item","Item Code","Item Code (CPC)").show(500,false)
+    df.groupBy("Element Code","Element").agg(count(lit(1)).alias("NumOfElements")).sort("Element","Element Code").show(500,false)
+    df.groupBy("Unit").agg(count(lit(1)).alias("NumOfUnits")).sort("Unit").show(300,false)
     
   }
 }
